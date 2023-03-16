c3b3c1a19e7e4f5fa12cd7415ea1d99471b17d07
itrsbj <122070749+itrsbj@users.noreply.github.com>
2023-03-16 15:42:12 +0800

    16-3

---
+++ foreign key.sql
@@ -0,0 +1,9 @@
+SELECT * FROM aug_2022.reg_periksa WHERE no_rawat = '2022/12/16/00025'
+
+delete 
+
+use aug_
+
+SET FOREIGN_KEY_CHECKS = 1
+
+DELETE  FROM reg_periksa WHERE no_rawat = '2022/12/16/00025'
